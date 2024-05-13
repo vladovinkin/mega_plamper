78186f5327c276eef540258a6a31c93fb595bed0
vladislav@book <vladovinkin@gmail.com>
2024-05-13 21:23:00 +0300

    - установил библиотеку vlucas/phpdotenv; - удалил старый файл с параметрами подключения; - настроил чтение параметоров подключения и секретов из .env файла

--- src/Common/Database/ConnectionProvider.php
+++ src/Common/Database/ConnectionProvider.php
@@ -3,22 +3,23 @@ declare(strict_types=1);
 
 namespace App\Common\Database;
 
+use Dotenv\Dotenv;
+
 final class ConnectionProvider
 {
-    const DATABASE_CONFIG_NAME = 'db.ini';
-
     public static function getConnection(): Connection
     {
         static $connection = null;
         if ($connection === null)
         {
-            // TODO: Добавить поддержку .env, чтобы упростить запуск примера в Windows
-//            $dsn = self::getEnvString('APP_DATABASE_DSN');
-//            $user = self::getEnvString('APP_DATABASE_USER');
-//            $password = self::getEnvString('APP_DATABASE_PASSWORD');\
-            $dsn = 'mysql:host=127.0.0.1;port=3326;dbname=megaplamper';
-            $user = 'megaplamper';
-            $password = 'LlcFZi8p';
+            $dotenv = Dotenv::createImmutable(__DIR__.'/../../../');
+            $dotenv->safeLoad();
+            $dotenv->required(['APP_DATABASE_DSN', 'APP_DATABASE_USER', 'APP_DATABASE_PASSWORD']);
+
+            $dsn = self::getEnvString('APP_DATABASE_DSN');
+            $user = self::getEnvString('APP_DATABASE_USER');
+            $password = self::getEnvString('APP_DATABASE_PASSWORD');
+
             $connection = new Connection($dsn, $user, $password);
         }
         return $connection;
@@ -26,7 +27,7 @@ final class ConnectionProvider
 
     private static function getEnvString(string $name): string
     {
-        $value = getenv($name);
+        $value = $_ENV[$name];
         if ($value === false)
         {
             throw new \RuntimeException("Environment variable '$name' not set");
